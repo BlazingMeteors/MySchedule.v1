acd1eabc6dd3bd777158a5d89d4b61a14391044e
BlazingMeteors <zhuya1222@163.com>
2022-06-29 00:17:07 +0800

    first commit:WPF界面布置

---
+++ MySchedule/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using Prism.DryIoc;
+using Prism.Ioc;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace MySchedule
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : PrismApplication
+    {
+        protected override Window CreateShell()
+        {
+            //启动窗口
+            return Container.Resolve<MainWindow>();
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            
+        }
+    }
+}
